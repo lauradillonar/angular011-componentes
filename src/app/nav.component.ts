34ae69258905f77fe3371fa9e3342fd831673e42
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-13 19:20:10 -0300

    Creando componente con templete externa

--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -2,14 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({ 
     selector: 'app-nav',
-    template: `
-        <h1>Hola desde mi primer componente creado manualmente</h1>
-        <p>Parrafo azul</p>
-        `,
-    styles: [`
-        h1{color: red;}
-        p{color:blue;}
-        `]
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.css']
 }) 
 
 export class NavComponent implements OnInit{ 
