38f88f2b5c247fedc6209be1e699950fcbcff649
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-13 19:54:07 -0300

    Creando los componentes de una web clasica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { SectionComponent } from './section/section.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent,
+    HeaderComponent,
+    AsideComponent,
+    SectionComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
