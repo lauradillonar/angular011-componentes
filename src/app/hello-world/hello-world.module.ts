f459b5eb0d733808828b84b1a00ad368ca6f0af7
lauradillonar <laura.dillon.ar@gmail.com>
2022-03-16 22:41:58 -0300

    Añadiendo componente al modulo

--- src/app/hello-world/hello-world.module.ts
+++ src/app/hello-world/hello-world.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HelloWorldComponent } from './hello-world.component';
 
 @NgModule({
   declarations: [
   
+  
+    HelloWorldComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HelloWorldComponent
   ]
 })
 export class HelloWorldModule { }
